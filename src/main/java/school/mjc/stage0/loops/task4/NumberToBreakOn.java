02fe157bde964e10b91813572302cd58d14e99c1
zurazumba <zura.zumbadze@yahoo.com>
2023-06-27 02:00:50 +0400

    loopskey

--- src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
+++ src/main/java/school/mjc/stage0/loops/task4/NumberToBreakOn.java
@@ -2,6 +2,26 @@ package school.mjc.stage0.loops.task4;
 
 public class NumberToBreakOn {
     public void printNumbersUntilWithBreakOn(int toBreakWith, int numberToGoUntil) {
+        if (toBreakWith > numberToGoUntil){
+            System.out.println("iterating till the end");
+            for (int i = 1; i <= numberToGoUntil; i++){
+                System.out.println(i);
+            }
+        }
+        else {
+            for (int j = 1; j <=numberToGoUntil; j++){
+                if (j==toBreakWith+1){
+                    break;
+                }
+                System.out.println(j);
+            }
+        }
 
+    }
+    public static void main (String [] args) {
+        NumberToBreakOn numberToBreakOn = new NumberToBreakOn();
+        int first = 5;
+        int second = 10;
+        numberToBreakOn.printNumbersUntilWithBreakOn(first, second);
     }
 }
--- src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
+++ src/main/java/school/mjc/stage0/loops/task4/SkippingThird.java
@@ -2,6 +2,13 @@ package school.mjc.stage0.loops.task4;
 
 public class SkippingThird {
     public void printUntilButThird(int lastPrinted) {
+//        lastPrinted = 7;
+        for (int i=0; i <= lastPrinted; i++){
+            if (i%3==0){
+                continue;
+            }
+            System.out.println(i);
+        }
 
     }
 }
